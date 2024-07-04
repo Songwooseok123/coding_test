ee95a0f01352f4753ef8b71f37acc343110aad32
송우석 <80091008+Songwooseok123@users.noreply.github.com>
2024-07-04 17:14:38 +0900

    Update README.sql

--- SQL_프로그래머스/README.sql
+++ SQL_프로그래머스/README.sql
@@ -1,6 +1,10 @@
 ### DATE_FOMAT(A, '날짜 어떻게 표시할지')
 SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
 
+-- select date_format('2022-07-16 19', '%Y%m%d%H');
+-- "2022071619"
+-- y: 두자리연도
+
 ### JOIN
 FROM USED_GOODS_REPLY R
     LEFT JOIN USED_GOODS_BOARD B
@@ -12,3 +16,18 @@ FROM USED_GOODS_REPLY R
 
 ### 오름차순 -- 이중 오름 차순 
 ORDER BY R.CREATED_DATE, B.TITLE;
+
+### 내림차순  
+ORDER BY CREATED_DATE DESC
+### A는 내림, B는 오름
+ORDER BY A DESC, B
+
+### WHERE 조건문 
+WHERE MONTH(DATE_OF_BIRTH) = 03 AND GENDER = 'W' AND TLNO IS NOT NULL
+
+-- AND로 계속 이어쓸 수 있음
+-- IS NOT NULL 문법 
+
+### WHERE OR 이나 WHERE IN이랑 같음     
+WHERE MCDP_CD = 'CS' OR MCDP_CD ='GS' 
+WHERE MCDP_CD in ('CS', 'GS')
