306f583b990c1b7ca9fa35e605795880a95c7811
송우석 <80091008+Songwooseok123@users.noreply.github.com>
2024-07-12 02:26:54 +0900

    Update README.py

--- Python_프로그래머스/README.py
+++ Python_프로그래머스/README.py
@@ -106,9 +106,18 @@ deque([4, 3])
 '''
 
 # continue 사용법
-![Untitled](https://prod-files-secure.s3.us-west-2.amazonaws.com/aa37adda-a925-4f98-92fd-edf6a1f56d7c/ac7b6a32-04ec-4db9-9806-131cc5f2e538/Untitled.png)
 ## if 문 만족하면 해당 i에 대한 for문이 끝나고 다음 i로 넘어감
 ## if 문 만족 안하면 밑에 print 실행함
+
+arr = [1,1,3,3,0,1,1] 
+for i in arr:
+    if i==1:
+        continue
+    print(i,"야호")
+
+# 문자열 TO 숫자열 
+ord('a')
+
 
 
 
