bb3874bfee4b9e2ae04a518a76877328f6f854e2
송우석 <80091008+Songwooseok123@users.noreply.github.com>
2024-07-11 23:14:39 +0900

    Update README.py

--- Python_프로그래머스/README.py
+++ Python_프로그래머스/README.py
@@ -1,9 +1,10 @@
-# sort()를 len을 기준으로 할 수 있음
+# 정수 띄어쓰기로 input 받는거
+list(map(int,input().split()))
 
+# sort()를 len을 기준으로 할 수 있음
 phone_book.sort(key=len)
 
 # zip 함수 활용
-
 phone_book =[1,2,3]
 for p,q in zip(phone_book,phone_book[1:]):
   print(p,q)
@@ -12,3 +13,39 @@ for p,q in zip(phone_book,phone_book[1:]):
 
 # 문자열 q가 p 문자열로 시작하는지
 q.startswith(p)
+
+# 리스트 요소 삭제 - 처음 발견한(앞에서부터 탐색함) 1개만 삭제됨 
+nums = [1,2,3,2,3]
+nums.remove(2)
+
+# 요소 갯수세기 - dictionary반환(많은 요소부터 정렬해서 반)
+from collections import Counter
+nums = [1,2,3,2,3]
+numsss = [1,2,3,4,3,3]
+
+Counter(nums)
+## Counter({3: 3, 2: 2, 1: 1, 4: 1})
+Counter(nums)
+## Counter({3: 3, 1: 1, 2: 1, 4: 1})
+
+## Counter 끼리 연산도 가능
+Counter(nums) - Counter(numsss)
+### Counter({2: 1})
+
+## Counter해서 가장 많이 등장하는 n개 찾기 - 리스트 안에 튜플로 반환
+b = Counter(nums)
+b.most_common(2)
+[(3, 3), (1, 1)]
+
+# 순열과 조합 
+from itertools import comninations,permutations # 순서대로 조합과 순열 
+list(combinations(nums,2))
+
+# set끼리 연산 가능 
+set([1,2,3,3,3]) - set([1,3,4])
+# {2}
+
+
+
+ 
+
